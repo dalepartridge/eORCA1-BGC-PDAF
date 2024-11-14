b5bf6a78529051ec78d2579a5d3cfb4d68d4b066
Yumeng Chen <yumeng.chen@reading.ac.uk>
2024-11-14 15:13:39 +0000

    update experiment setup scripts;

---
+++ cycle/set_environment-chlo-pc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Config options
+export WORK=/work/n01/n01/ymchen/eORCA1-BGC-PDAF
+export EXP_NAME=EXP-MEDUSA_chlo_pc
+export RUN_DIR=$WORK/RUN/$EXP_NAME
+export NAME=eORCA1
+
+export ystart=2015
+export yend=2020
+# starting the experiment from 1st Feb. 2015
+export starting_iter=526944 # 525952 # starting iteration
+
+# Default directories
+export DEFAULT_RUN_DIR=$WORK/RUN/EXP00_MEDUSA
+export INPUT_DIR=$WORK/INPUTS
+export OUTPUT_DIR=$WORK/OUTPUTS
+export CYCLE_DIR=$WORK/cycle
+
